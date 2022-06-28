c2a31364bf206a837f06a07c552d0fb3303ab4c0
ygritos <ygritos@outlook.com>
2022-06-28 20:03:23 +0500

    Emendations

--- src/Main.java
+++ src/Main.java
@@ -1,71 +1,56 @@
-import java.text.ParseException;
 import java.util.Scanner;
-import java.text.DecimalFormat;
-
 public class Main {
+//* Exiting program in case of wrong input
     public static void wrongExit() {
         System.out.println("Wrong input!");
         System.exit(0);
     }
+//* Converting digital part of input to number type
+    public static double stringToNumber(String num_) {
+        var number = Integer.parseInt(num_);
+        if (number > 10)
+            wrongExit();
+        return number;
+    }
     public static void main(String[] args) {
-
-        //  App description
-        System.out.println("Calculator 2.0 - can perform 4 basic arithmetic operations");
+//  App description
+        System.out.println("\nCalculator 2.0 - can perform 4 basic arithmetic operations");
         System.out.println("(addition, subtraction, multiplication and division)");
-        System.out.println("between 3 (or 2) integer numbers from 1 to 10 inclusive");
-        System.out.println();
-
-        //  Explanation for user
-        System.out.println("Enter math expression in format 'a X b X c' or 'a X b'");
-        System.out.println("where a, b, c are integers in range from 1 to 10 inclusive");
+        System.out.println("between 2 or more integer numbers from 1 to 10 inclusive");
+//  Explanation for user
+        System.out.println("\nEnter math expression in format 'a X b X c ...' ");
+        System.out.println("where a, b, c ... are integers in range from 1 to 10 inclusive");
         System.out.println("and X is any of 4 arithmetic operations: +, -, * or /");
-
-        //  Processing user's input
-        Scanner scanner = new Scanner(System.in);
-        String mathExpr = scanner.nextLine();
-        mathExpr = mathExpr.replace(" ", "");
-        if (!mathExpr.startsWith("-"))
-            mathExpr = "+" + mathExpr;
-
-        //  Declaring necessary variables
-        DecimalFormat decimalFormat = new DecimalFormat("#");
-        String[] monomial = mathExpr.split("(?=[+-])");
-        String[] factor;
-        int power;
-        double result = 0;
-
-        //  Solving problem
-        for (String mon_ : monomial) {
-            if (mon_.contains("*") || mon_.contains("/")) {
-                factor = mon_.split("(?=[*/])");
-                result = 1;
-                for (String fac_ : factor) {
-                    power = 1;
-                    if (fac_.startsWith("/"))
+        System.out.println("\nOr type \"q\" for exit");
+//  Problem Solving
+        while (true) {
+//  -   Processing user's input
+            var input = new Scanner(System.in);
+            var mathExpr = input.nextLine();
+            mathExpr = mathExpr.replace(" ", "");
+            if (mathExpr.equals("q"))
+                break;
+            if (mathExpr.startsWith("*") || mathExpr.startsWith("/"))
+                wrongExit();
+            if (!(mathExpr.startsWith("-") || mathExpr.startsWith("+")))
+                mathExpr = "+" + mathExpr;
+//  -   Solving algorithm
+            String[] polynomial = mathExpr.split("(?=[+-])");
+            double result = 0;
+            for (String pol_ : polynomial) {
+                String[] monomial = pol_.split("(?=[*/])");
+                double product = 1;
+                for (String mon_ : monomial) {
+                    byte power = 1;
+                    if (mon_.startsWith("/"))
                         power = -1;
-                    else if (!(fac_.startsWith("*") || fac_.startsWith("+") || fac_.startsWith("-")))
-                        wrongExit();
-                    fac_ = fac_.substring(1);
-                    try {
-                        decimalFormat.parse(fac_.substring(1)).intValue();
-                    } catch (ParseException e) {
-                        throw new RuntimeException(e);
-                    }
-                    if (Integer.parseInt(fac_) > 10)
-                        wrongExit();
-                    result *= Math.pow(Double.parseDouble(fac_), power);
+                    if (!mon_.startsWith("-"))
+                        mon_ = mon_.substring(1);
+                    product *= Math.pow(stringToNumber(mon_), power);
                 }
-            } else {
-                try {
-                    decimalFormat.parse(mon_.substring(1)).intValue();
-                } catch (ParseException e) {
-                    throw new RuntimeException(e);
-                }
-                if (Integer.parseInt(mon_) > 10)
-                    wrongExit();
-                else result += Double.parseDouble(mon_);
+                result += product;
             }
+            System.out.println(result);
         }
-        System.out.println(result);
     }
 }
